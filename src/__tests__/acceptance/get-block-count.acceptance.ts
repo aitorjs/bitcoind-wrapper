0a4714843b9c4f730bc45d3fe9752d4d35ad616b
aitorjs <aitiba@gmail.com>
2020-02-12 00:47:31 +0100

    test calling getblockcount without auth/JWT

--- src/__tests__/acceptance/get-block-count.acceptance.ts
+++ src/__tests__/acceptance/get-block-count.acceptance.ts
@@ -16,27 +16,9 @@ describe('HomePage', () => {
 
   it('getBlockCount', async () => {
     console.log('getBlockCount')
-   /*  // arrange
-    const product = await givenProduct({
-      name: 'Ink Pen',
-      slug: 'ink-pen',
-      price: 1,
-      category: 'Stationery',
-      description: 'The ultimate ink-powered pen for daily writing',
-      label: 'popular',
-      available: true,
-      endDate: null,
-    });
-    const expected = Object.assign({id: product.id}, product); */
+   /* const expected = Object.assign({id: product.id}, product); */
 
     const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkZjdhMmZhNjIyODM3MjEzODI2YWE2YiIsIm5hbWUiOiJVc2VyIE9uZSIsImlhdCI6MTU3NzQ2MDE3MCwiZXhwIjoxNjM3NDYwMTcwfQ.hJko5UGN-TaS58JokZpWkyeeljt9LcuNG1BwRyaaMrU"
-    // act
-
-    const response2 = await client
-    .get('/ping')
-    .then((res) => {
-      console.log('res', res.text)
-     });
 
     const response = await client
       .get('/bitcoin/getblockcount')
@@ -44,7 +26,7 @@ describe('HomePage', () => {
       .then((res) => {
         console.log('res', res.text)
        });
-    // assert
+
     // expect(response.body).to.containEql(expected); */
 
     // http://localhost:3000/bitcoin/getblockcount" -H "accept: application/json" -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkZjdhMmZhNjIyODM3MjEzODI2YWE2YiIsIm5hbWUiOiJVc2VyIE9uZSIsImlhdCI6MTU3NzQ2MDE3MCwiZXhwIjoxNjM3NDYwMTcwfQ.hJko5UGN-TaS58JokZpWkyeeljt9LcuNG1BwRyaaMrU
@@ -58,5 +40,4 @@ describe('HomePage', () => {
      expect(res.body.role).to.be.equal('customer');
     }); */
   });
-
 });
